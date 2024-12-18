f2de31f2ea587bd8a0c66c5cf463f68fa3f0fa20
ezgsnyrt <ezgi.senyurt3@gmail.com>
2024-12-18 16:31:18 +0100

    Added essential frontend packages for React project
    Included:
    - React and React-DOM for core functionality
    - React-Bootstrap and Bootstrap for styling
    - Font Awesome for icons
    - Sass and Normalize-SCSS for SCSS support and browser resets
    - React Router for navigation
    - TypeScript for type safety
    - Jest and Testing Library for testing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sass/main.scss';
+import HomePage from './pages/HomePage';
+
+
+function App() {
+  return (
+    <div className="App"> Hello, React!
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>NavBar details will be here.</div>
+  )
+}
+
+export default NavBar;
