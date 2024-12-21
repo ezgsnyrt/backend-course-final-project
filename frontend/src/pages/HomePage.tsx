6c72611ed0cc79482140354087a5d47ae8b3abeb
ezgsnyrt <ezgi.senyurt3@gmail.com>
2024-12-21 21:01:02 +0100

    Enhanced HomePage.tsx component by adding state management for doctors and users, fetching data dynamically from config.json, and passing the data as props to children(Dropdown.tsx & DoctorTable.tsx)

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
-import Dropdown from '../components/Dropdown';
+import React, { useState, useEffect } from "react";
+import NavBar from "../components/NavBar";
+import Dropdown from "../components/Dropdown";
+import DoctorTable from "../components/DoctorTable";
+import data from "../config/config.json";
+import { Doctor } from "../components/Types/doctorDropdown.interface";
 
 const HomePage = () => {
-  return (
-    <div>
-      <NavBar />
-      <Dropdown />
-    </div>
-  )
-}
+    // State for doctors and users
+    const [doctors, setDoctors] = useState<Doctor[]>([]);
+    const [users, setUsers] = useState<any[]>([]); // Adjust type if you have a Users interface
+
+    // Fetch doctors and users data
+    useEffect(() => {
+        // Simulate fetching data
+        setDoctors(data.doctors);
+        setUsers(data.users);
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            {/* Pass doctors data to Dropdown */}
+            <Dropdown doctors={doctors} />
+            {/* Pass doctors and users data to DoctorTable */}
+            <DoctorTable doctors={doctors} users={users} />
+        </div>
+    );
+};
 
 export default HomePage;
