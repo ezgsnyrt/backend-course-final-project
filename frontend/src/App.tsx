b49246d00feb32999ff96cfa9be2a5a39ac13209
ezgsnyrt <ezgi.senyurt3@gmail.com>
2024-12-21 15:40:11 +0100

    created NavBar.tsx component and added necessary links
    added their styles to NavBar.scss
    
    imported NavBar.tsx into HomePage.tsx
    
    refactored NavBar.scss to ensure responsive design
    
    updated _colors.scss accordingly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App"> Hello, React!
+    <div className="App">
       <HomePage />
     </div>
   );
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMd, faHospital, faUsers } from "@fortawesome/free-solid-svg-icons";
 
-const NavBar = () => {
-  return (
-    <div>NavBar details will be here.</div>
-  )
-}
+const NavBar: React.FC = () => {
+    return (
+        <Navbar
+            expand="lg"
+            className="navBar"
+            sticky="top"
+        >
+            <Container>
+                <Navbar.Brand href="#home" className="brand">
+                    <FontAwesomeIcon icon={faHospital} className="me-2" />
+                    DocApp
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="navbar-nav"
+                    aria-label="Toggle navigation"
+                />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home" className="navLink">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link href="#doctors" className="navLink">
+                            <FontAwesomeIcon icon={faUserMd} className="me-2" />
+                            Doctors
+                        </Nav.Link>
+                        <Nav.Link href="#users" className="navLink">
+                            <FontAwesomeIcon icon={faUsers} className="me-2" />
+                            Users
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
 
 export default NavBar;
