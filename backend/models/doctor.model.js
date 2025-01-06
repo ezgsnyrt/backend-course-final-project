613e3a0dc18f8c9dcfe8513cd02bd4bfbe5892c3
ezgsnyrt <ezgi.senyurt3@gmail.com>
2025-01-07 00:03:01 +0100

    Deleted models folder and integrated models into specific schema files as previously done

--- backend/models/doctor.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const doctorSchema = require('../schemas/doctor.schema');
-
-
-const Doctor = mongoose.model("doctor", doctorSchema);
-module.exports = Doctor;
--- backend/schemas/doctor.schema.js
+++ backend/schemas/doctor.schema.js
@@ -39,3 +39,6 @@ const doctorSchema = new mongoose.Schema(
         }
     }
 )
+
+const Doctor = mongoose.model("doctor", doctorSchema);
+module.exports = Doctor;
